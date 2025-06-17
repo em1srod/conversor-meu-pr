ad962cc7dff81a083873d898a8eb04326bdc7a6e
emillyalves <147439821+em1srod@users.noreply.github.com>
2025-06-16 21:49:04 -0300

    Atualização do arquivo javascript

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-// conversor-meu-pr JS
-console.log('conversor-meu-pr script loaded');
+function converter() {
+  const valor = parseFloat(document.getElementById('valor').value);
+  const tipo = document.getElementById('tipoConversao').value;
+  let resultado = 0;
+
+  if (isNaN(valor)) {
+    document.getElementById('resultado').innerText = "Digite um valor válido.";
+    return;
+  }
+
+  if (tipo === "kgToLb") {
+    resultado = valor * 2.20462;
+    document.getElementById('resultado').innerText = `${valor} kg = ${resultado.toFixed(2)} lb`;
+  } else {
+    resultado = valor * 0.453592;
+    document.getElementById('resultado').innerText = `${valor} lb = ${resultado.toFixed(2)} kg`;
+  }
+}
